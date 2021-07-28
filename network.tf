c45165b765b28f5050d5b26eacd529ce7f878a0c
ladan19 <ladandecsm@gmail.com>
2021-07-28 13:34:00 +0000

    compute

--- network.tf
+++ network.tf
@@ -9,6 +9,7 @@ resource "oci_core_subnet" "test_subnet" {
 	cidr_block = "10.0.0.0/24"
 	vcn_id = oci_core_vcn.vcn_test.id
 	display_name = "subnet-public"
+	route_table_id = oci_core_route_table.test_route_table.id
 }
 resource "oci_core_internet_gateway" "test_internet_gateway" {
 
