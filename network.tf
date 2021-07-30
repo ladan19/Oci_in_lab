3d2ef63b91645533b73c0389296cd66dc53e7f5a
ladan19 <ladandecsm@gmail.com>
2021-07-30 12:53:41 +0000

    route table

--- network.tf
+++ network.tf
@@ -9,9 +9,21 @@ resource "oci_core_subnet" "test_subnet" {
 	compartment_id = "ocid1.compartment.oc1..aaaaaaaaqngvj3a4zn3bkprl4ygqr7qjahsb2lrzc647yk7kowlpvem2nsma"
 	vcn_id = oci_core_vcn.test_vcn.id
 	display_name = "public-subnet"
+	route_table_id = oci_core_route_table.test_route_table.id
+	
 }
 
 resource "oci_core_internet_gateway" "test_internet_gateway" {
 	compartment_id = var.compartment
 	vcn_id = oci_core_vcn.test_vcn.id
+}
+
+resource "oci_core_route_table" "test_route_table" {
+	compartment_id = var.compartment
+	vcn_id = oci_core_vcn.test_vcn.id
+
+	route_rules {
+		network_entity_id = oci_core_internet_gateway.test_internet_gateway.id
+		destination = "0.0.0.0/0"
+		}
 }
