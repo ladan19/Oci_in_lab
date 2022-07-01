58c4364d0fe8765c4ba194d3ba80a35cac539f81
Ladan Schulte Machado <61203941+ladan19@users.noreply.github.com>
2022-07-01 13:12:18 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,3 +2,11 @@ variable "compartment" {
 	type = string
 	default = "ocid1.compartment.oc1..aaaaaaaaqngvj3a4zn3bkprl4ygqr7qjahsb2lrzc647yk7kowlpvem2nsma"
 }
+
+
+
+
+variable "tenancy_ocid" {
+	type = string
+	default = "ocid1.tenancy.oc1..aaaaaaaabvf3ovdefp4kcpkamvudoail2dczpgl62rokiqgc35xheqld2dba"
+}
