589011be738ec382d6c2d6c92bc5238101c65593
Ladan Schulte Machado <61203941+ladan19@users.noreply.github.com>
2022-07-01 13:34:35 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 provider "oci" {
            version = ">= 3.27.0"
            tenancy_ocid = var.tenancy_ocid
+           user_ocid = {}
+           oci_private = {}
+           oci_fingerprint = {}
+           
 }
