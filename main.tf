c4f7cde9b8acda966ccb4e547422187eb6c38415
Ladan Schulte Machado <61203941+ladan19@users.noreply.github.com>
2022-07-01 13:51:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 provider "oci" {
            version = ">= 3.27.0"
+           region= sa-saopaulo-1
            tenancy_ocid = var.tenancy_ocid
            user_ocid   = var.user_ocid
            fingerprint  = var.fingerprint
