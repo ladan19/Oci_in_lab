60a7b0b4b1501b378762f17a7cbdc7cf9a4b6b40
Ladan Schulte Machado <61203941+ladan19@users.noreply.github.com>
2022-07-01 12:40:56 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "region" {}
-
 provider "oci" {
-   tenancy_ocid = "${var.tenancy_ocid}"
+   tenancy_ocid = ${{ secrets.OCI_COMPARTMENT_OCID }}
    user_ocid = "${var.user_ocid}"
    fingerprint = "${var.fingerprint}"
    private_key_path = "${var.private_key_path}"
